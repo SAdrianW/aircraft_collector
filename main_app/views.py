1b058d483cec88cbc1c3b19f06bdb7272f861cd1
SAdrianW <wellman.shaun@gmail.com>
2023-08-11 15:39:01 +1000

    added about and aircraft index pages. minor style changes with materialize

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
 
+aircraft_list = [
+    {'name': 'A-10 Thunderbolt', 'role': 'Close Air Support', 'capacity': 'GUA-8 Avenger Gatling Cannon', 'size': 'L: 16.2m W: 17.5m H: 4.5m'},
+    {'name': 'C-17 Globemaster', 'role': 'Transport', 'armament-capacity': '77,519kg cargo', 'size': 'L: 53m W: 51.7m H: 16.8m'}
+]
+
 # Create your views here.
+def home(request):
+    return render(request, 'home.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def aircraft_index(request):
+    return render(request, 'aircraft/index.html', {
+        'aircraft_list': aircraft_list
+    })
