fdf717a2f1d5c3dd43c0d233ca1a3405c3f2242d
SAdrianW <wellman.shaun@gmail.com>
2023-08-11 14:08:36 +1000

    rename of files

--- aircraftcollector/settings.py
+++ aircraftcollector/settings.py
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 'planecollector.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'planecollector',
+        'NAME': 'aircraftcollector',
     }
 }
 
